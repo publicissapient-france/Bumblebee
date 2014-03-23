1035d03a03963156820ac03402a20ce63729953d
mbreton <mbreton@xebia.fr>
2014-03-23 23:19:02 +0100

    Fix gradle build and add assets donwloads

--- src/main/java/fr/xebia/bumblebee/CameraTest.java
+++ src/main/java/fr/xebia/bumblebee/CameraTest.java
@@ -1,21 +1,25 @@
 package fr.xebia.bumblebee;
 
+import com.googlecode.javacpp.Loader;
 import com.googlecode.javacv.CanvasFrame;
 import com.googlecode.javacv.OpenCVFrameGrabber;
 import com.googlecode.javacv.cpp.opencv_core.*;
 
 import java.io.File;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import static com.googlecode.javacv.cpp.opencv_core.*;
 import static com.googlecode.javacv.cpp.opencv_imgproc.*;
 
 public class CameraTest {
-    static int hueLowerR = 0;
-    static int hueUpperR = 20;
+    static int minHeight = 20;
+    static int minWidth = 10;
 
     public static void main(String[] args) throws Exception {
-        URL videoUrl = CameraTest.class.getResource("cut.mov");
+        URL videoUrl = CameraTest.class.getResource("/output.mov");
         File video = new File(videoUrl.toURI());
         OpenCVFrameGrabber grabber = new OpenCVFrameGrabber(video);//"resources/main/cut.mov");
         grabber.start();
@@ -26,13 +30,12 @@ public class CameraTest {
         grabber.setFrameRate(grabber.getFrameRate());
 
         // source init
-//        CanvasFrame sourceCanvas = new CanvasFrame("Source");
-//        sourceCanvas.setCanvasSize(width, height);
-        IplImage frame;
+        CanvasFrame sourceCanvas = new CanvasFrame("Source");
+        sourceCanvas.setCanvasSize(width, height);
 
         // post-treatment init
         CanvasFrame outputcanvas = new CanvasFrame("Post-treatment");
-        outputcanvas.setCanvasSize(width, height);
+        sourceCanvas.setCanvasSize(width, height);
 
         IplImage outputImg = null;
         IplImage hsvImg = null;
@@ -43,14 +46,18 @@ public class CameraTest {
         // background subtraction
         //opencv_video.BackgroundSubtractorMOG2 pMOG2 = new opencv_video.BackgroundSubtractorMOG2(30, 16, true); //MOG2 approach
 
-        while (outputcanvas.isVisible() /*&& sourceCanvas.isVisible() */&& (frame = grabber.grab()) != null) {
-            if (outputImg == null) {
-                outputImg = IplImage.create(cvGetSize(frame), 8, 1);
-            }
-            //cvCvtColor(frame, outputImg, CV_BGR2GRAY);
-            //pMOG2.apply(outputImg, outputImg, -1);
-            //GaussianBlur(outputImg, outputImg, new opencv_core.CvSize(3, 3), 0, 0, BORDER_DEFAULT);
-            //cvAdaptiveThreshold(outputImg, outputImg, 255, CV_ADAPTIVE_THRESH_GAUSSIAN_C, CV_THRESH_BINARY_INV, 5, 4);
+        boolean canGrab = true;
+        try {
+            while (outputcanvas.isVisible() && sourceCanvas.isVisible() && canGrab) {
+                final IplImage frame = grabber.grab();
+                canGrab = frame != null;
+                if (outputImg == null) {
+                    outputImg = IplImage.create(cvGetSize(frame), 8, 1);
+                }
+                //cvCvtColor(frame, outputImg, CV_BGR2GRAY);
+                //pMOG2.apply(outputImg, outputImg, -1);
+                //GaussianBlur(outputImg, outputImg, new opencv_core.CvSize(3, 3), 0, 0, BORDER_DEFAULT);
+                //cvAdaptiveThreshold(outputImg, outputImg, 255, CV_ADAPTIVE_THRESH_GAUSSIAN_C, CV_THRESH_BINARY_INV, 5, 4);
             /*CvMemStorage mem = CvMemStorage.create();
             double distanceResolutionInPixels = 1;
             double angleResolutionInRadians = Math.PI / 180;
@@ -104,30 +111,58 @@ public class CameraTest {
                 }
             }*/
 
-            if (hsvImg == null) {
-                hsvImg = cvCreateImage(cvGetSize(frame), 8, 3);
-                redImg = cvCreateImage(cvGetSize(frame), 8, 1);
-                greenImg = cvCreateImage(cvGetSize(frame), 8, 1);
-                blueImg = cvCreateImage(cvGetSize(frame), 8, 1);
+                if (hsvImg == null) {
+                    hsvImg = cvCreateImage(cvGetSize(frame), 8, 3);
+                    redImg = cvCreateImage(cvGetSize(frame), 8, 1);
+                    greenImg = cvCreateImage(cvGetSize(frame), 8, 1);
+                    blueImg = cvCreateImage(cvGetSize(frame), 8, 1);
+                }
+                cvSplit(frame, blueImg, greenImg, redImg, null);
+                cvAdd(blueImg, greenImg, greenImg, null);
+                cvSub(redImg, greenImg, redImg, null);
+                cvThreshold(redImg, redImg, 20, 255, CV_THRESH_BINARY);
+
+                CvMemStorage mem = CvMemStorage.create();
+                CvSeq contours = new CvSeq();
+                CvSeq ptr;
+                cvFindContours(redImg, mem, contours, Loader.sizeof(CvContour.class), CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE);
+
+                List<CvRect> rectangles = new ArrayList<>();
+                for (ptr = contours; ptr != null; ptr = ptr.h_next()) {
+                    rectangles.add(cvBoundingRect(ptr, 0));
+                }
+                Optional<CvRect> optPhoneRect = rectangles.stream()
+                        .filter(rect -> rect.height() > minHeight && rect.width() > minWidth)
+                        .findFirst();
+
+                if (optPhoneRect.isPresent()) {
+                    CvRect phoneRect = optPhoneRect.get();
+                    IplImage cropped = cvCreateImage(cvSize(phoneRect.width(), phoneRect.height()), IPL_DEPTH_8U, 3);
+                    IplImage croppedGray = cvCreateImage(cvGetSize(cropped), IPL_DEPTH_8U, 1);
+                    cvCvtColor(cropped, croppedGray, CV_BGR2GRAY);
+                    IplImage croppedThresholded = croppedGray.clone();
+                    cvSetImageROI(frame, phoneRect);
+                    cvCopy(frame, cropped);
+                    cvResetImageROI(frame);
+
+                    //cvThreshold(cropped, cropped, 255, 255, CV_THRESH_BINARY_INV);
+                    cvAdaptiveThreshold(croppedGray, croppedThresholded, 255, CV_ADAPTIVE_THRESH_MEAN_C, CV_THRESH_BINARY_INV, 5, 4);
+                    outputImg = croppedGray;//croppedThresholded;
+
+                    cvRectangle(frame, cvPoint(phoneRect.x(), phoneRect.y()),
+                            cvPoint(phoneRect.x() + phoneRect.width(), phoneRect.y() + phoneRect.height()),
+                            CvScalar.BLUE, 1, 0, 0);
+
+                    sourceCanvas.showImage(frame);
+                    outputcanvas.setCanvasSize(outputImg.width() * 5, outputImg.height() * 5);
+                    outputcanvas.showImage(outputImg);
+                }
             }
-            cvCvtColor(frame, hsvImg, CV_BGR2HSV);
-            cvSplit(frame, blueImg, greenImg, redImg, null);
-            cvAdd(blueImg, greenImg, greenImg, null);
-            cvSub(redImg, greenImg, redImg, null);
-            cvThreshold(redImg, redImg, 20, 255, CV_THRESH_BINARY);
-
-            cvInRangeS(hsvImg,
-                    cvScalar(hueLowerR, 100, 99, 0),
-                    cvScalar(hueUpperR, 61, 26, 0),/*cvScalar(8, 85, 85, 0), cvScalar(353, 56, 53, 0),*/
-                    outputImg);
-
-
-            //sourceCanvas.showImage(frame);
-            outputcanvas.showImage(redImg);
+        } finally {
+            grabber.stop();
+            outputcanvas.dispose();
+            sourceCanvas.dispose();
         }
-        grabber.stop();
-        outputcanvas.dispose();
-        //sourceCanvas.dispose();
     }
 
     static CvPoint computeIntersect(CvPoint a1, CvPoint a2, CvPoint b1, CvPoint b2) {
